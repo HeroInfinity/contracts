9626bbe4fd42ca1ebc3bdcc99c017d390b0e3cdd
mingcrypto <mingli970909@gmail.com>
2022-02-24 18:14:16 +1000

    Update token contract

--- contracts/HeroInfinityToken.sol
+++ contracts/HeroInfinityToken.sol
@@ -2,36 +2,10 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract HeroInfinityToken is ERC20, Ownable {
-  mapping (address => bool) private bots;
-
+contract HeroInfinityToken is ERC20 {
   constructor() ERC20("Hero Infinity Token", "HRI") {
     _mint(msg.sender, 10**(9 + 18)); // 1B total supply
-  }
-
-  function _transfer(
-    address sender,
-    address recipient,
-    uint256 amount
-  ) internal override {
-    require(amount > 0, "HRI: transfer amount must be greater than zero");
-    require(!bots[sender] && !bots[recipient], "HRI: transfer by bot");
-
-    super._transfer(sender, recipient, amount);
-  }
-
-  function setBots(address[] memory addrs) public onlyOwner {
-    for (uint i = 0; i < addrs.length; i++) {
-      bots[addrs[i]] = true;
-    }
-  }
-
-  function unsetBots(address[] memory addrs) public onlyOwner {
-    for (uint i = 0; i < addrs.length; i++) {
-      bots[addrs[i]] = false;
-    }
   }
 }
