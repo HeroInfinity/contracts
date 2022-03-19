cff07d9dd6670cc3bc5b053434a98a5d5937c383
mingcrypto <mingli970909@gmail.com>
2022-03-20 00:24:11 +1000

    use compiler 0.8.9

--- contracts/TokenGovernor.sol
+++ contracts/TokenGovernor.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.13;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
