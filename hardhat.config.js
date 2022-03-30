15c82b3485a4ab6696bec1a9fe4fad565d37d976
mingcrypto <mingli970909@gmail.com>
2022-03-30 22:27:37 +1000

    update hardhat solidity optimize

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,7 +23,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     mainnet: {
       url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161	",
